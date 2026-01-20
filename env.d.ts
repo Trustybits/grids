929022ffb2e67f08ed3ffee0ca53455075380321
Cam <cam@trustybits.com>
2026-01-19 23:27:02 -0700

    Added/Installed Posthog to Grids for analytics and tracking.

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 declare module 'vue3-grid-layout';
+
+interface ImportMetaEnv {
+  readonly VITE_POSTHOG_KEY?: string;
+  readonly VITE_POSTHOG_HOST?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

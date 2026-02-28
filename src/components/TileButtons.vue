4105c81baec8fe809b10697adc22c6ef4063e4b9
Cam W <cam@trustybits.com>
2026-02-27 17:47:28 -0700

    chore: Comment out roadmap feed button and remove unused commented buttons from tile toolbar
    - Comment out roadmap feed button in TileButtons.vue toolbar so that other users can't use the tile quite yet
    - Remove commented-out emoji buttons (📽, 🎵, 📌) that were never implemented
    - Keep RPG button commented out as before

--- src/components/TileButtons.vue
+++ src/components/TileButtons.vue
@@ -25,9 +25,6 @@
       <button class="btn btn-secondary" data-tooltip="Link" @click="addLinkElement">
         <LinkIcon />
       </button>
-      <!-- <button class="btn btn-secondary" @click="addLinkElement">📽</button>
-      <button class="btn btn-secondary" @click="addLinkElement">🎵</button>
-      <button class="btn btn-secondary" @click="addLinkElement">📌</button> -->
       <button class="btn btn-secondary" data-tooltip="Embed" @click="addEmbedElement">
         <EmbedIcon />
       </button>
@@ -37,14 +34,13 @@
       <button class="btn btn-secondary" data-tooltip="Campfire" @click="addCampfireElement">
         <CampfireIcon />
       </button>
-      <button class="btn btn-secondary" data-tooltip="Roadmap" @click="addRoadmapFeedElement">
-        <!-- Clipboard-list icon representing a roadmap/task list -->
+      <!-- <button class="btn btn-secondary" data-tooltip="Roadmap" @click="addRoadmapFeedElement">
         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M9 5H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           <rect x="9" y="3" width="6" height="4" rx="1" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           <path d="M9 12h6M9 16h4" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
-      </button>
+      </button> -->
       <!-- <button class="btn btn-secondary" @click="addRPGElement">
         <RPGIcon />
       </button> -->

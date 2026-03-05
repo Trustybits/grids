fb0a5fbaf7470486a4fef6a3f62b70f4e991f8e3
Cam W <cam@trustybits.com>
2026-03-04 17:01:44 -0700

    refactored grid menu icon into separate component

--- src/components/GridMenu.vue
+++ src/components/GridMenu.vue
@@ -6,16 +6,7 @@
       @click.stop="toggleMenu"
     >
       <div class="grid-menu-icon">
-        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066-2.573c-.94-1.543.826-3.31 2.37-2.37a1.724 1.724 0 0 0 2.572-1.065Z"
-            stroke="currentColor"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <circle cx="12" cy="12" r="3" stroke="currentColor" stroke-width="1.5" />
-        </svg>
+        <GridMenuIcon />
       </div>
 
       <div class="grid-menu-dropdown" v-if="showMenu" @click.stop>
@@ -93,6 +84,7 @@ import Toggle from "./Toggle.vue";
 import Accordion from "./Accordion.vue";
 import MenuSection from "./MenuSection.vue";
 import Divider from "./Divider.vue";
+import GridMenuIcon from "./icons/GridMenuIcon.vue";
 
 const emit = defineEmits<{
   "confirm-delete": [];

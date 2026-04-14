179d576e152b986301f78794930e3a4ce2c61fe9
tyler-bartschi <tyler@trustybits.com>
2026-04-14 08:50:50 -0600

    removed old FirestoreLayoutService

--- src/services/LayoutServiceFactory.ts
+++ src/services/LayoutServiceFactory.ts
@@ -5,7 +5,7 @@ const useMockData = false;
 
 export const getLayoutService = (): ILayoutService => {
   if (useMockData) {
-    // return new MockLayoutService();
+    return new MockLayoutService();
   }
 
   return new LayoutService();

d43f4e0204d5bd475a00c381db3aa0c2d63dbd39
tyler-bartschi <tyler@trustybits.com>
2026-04-16 12:14:39 -0600

    added mocks for the Storage and User services, built the StorageService using the StorageDao

--- src/services/factory/IServiceFactory.ts
+++ src/services/factory/IServiceFactory.ts
@@ -1,7 +1,9 @@
 import type { ILayoutService } from "../interfaces/ILayoutService";
+import type { IStorageService } from "../interfaces/IStorageService";
 import type { IUserService } from "../interfaces/IUserService";
 
 export interface IServiceFactory {
   getLayoutService: () => ILayoutService;
+  getStorageService: () => IStorageService;
   getUserService: () => IUserService;
 }
--- src/services/factory/ServiceFactory.ts
+++ src/services/factory/ServiceFactory.ts
@@ -1,13 +1,18 @@
 import { LayoutService } from "../LayoutService";
+import { StorageService } from "../StorageService";
 import { UserService } from "../UserService";
 import type { ILayoutService } from "../interfaces/ILayoutService";
+import type { IStorageService } from "../interfaces/IStorageService";
 import type { IUserService } from "../interfaces/IUserService";
 import { MockLayoutService } from "../mocks/MockLayoutService";
+import { MockStorageService } from "../mocks/MockStorageService";
+import { MockUserService } from "../mocks/MockUserService";
 import type { IServiceFactory } from "./IServiceFactory";
 
 export class ServiceFactory implements IServiceFactory {
   private useMocks: boolean;
   private layoutService: ILayoutService;
+  private storageService: IStorageService;
   private userService: IUserService;
 
   public constructor(useMocks: boolean = false) {
@@ -15,16 +20,21 @@ export class ServiceFactory implements IServiceFactory {
 
     if (this.useMocks) {
       this.layoutService = new MockLayoutService();
-      // TODO: add MockUserService when one is created
-      this.userService = new UserService();
+      this.storageService = new MockStorageService();
+      this.userService = new MockUserService();
     } else {
       this.layoutService = new LayoutService();
+      this.storageService = new StorageService();
       this.userService = new UserService();
     }
   }
 
   public getLayoutService(): ILayoutService {
     return this.layoutService;
+  }
+
+  public getStorageService(): IStorageService {
+    return this.storageService;
   }
 
   public getUserService(): IUserService {

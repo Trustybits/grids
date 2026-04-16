9a4f4d1eb5dcd6d67a261e7b93ced19a957541d0
tyler-bartschi <tyler@trustybits.com>
2026-04-16 10:39:49 -0600

    built out a Storage DAO for file upload and persistence

--- src/dao/firestore/factory/FirestoreDaoFactory.ts
+++ src/dao/firestore/factory/FirestoreDaoFactory.ts
@@ -1,12 +1,14 @@
-import { db } from "@/firebase";
+import { db, storage } from "@/firebase";
 import type { ChatDao } from "@/dao/interfaces/ChatDao";
 import type { CustomerDao } from "@/dao/interfaces/CustomerDao";
 import type { DaoFactory } from "@/dao/interfaces/factory/DaoFactory";
 import type { LayoutDao } from "@/dao/interfaces/LayoutDao";
 import type { SlugDao } from "@/dao/interfaces/SlugDao";
+import type { StorageDao } from "@/dao/interfaces/StorageDao";
 import type { UpvoteDao } from "@/dao/interfaces/UpvoteDao";
 import type { UserDao } from "@/dao/interfaces/UserDao";
 import type { UserGameDataDao } from "@/dao/interfaces/UserGameDataDao";
+import { FirebaseStorageDao } from "../FirebaseStorageDao";
 import { FirestoreChatDao } from "../FirestoreChatDao";
 import { FirestoreCustomerDao } from "../FirestoreCustomerDao";
 import { FirestoreLayoutDao } from "../FirestoreLayoutDao";
@@ -20,6 +22,7 @@ export class FirestoreDaoFactory implements DaoFactory {
   private customerDao: CustomerDao;
   private layoutDao: LayoutDao;
   private slugDao: SlugDao;
+  private storageDao: StorageDao;
   private upvoteDao: UpvoteDao;
   private userDao: UserDao;
   private userGameDataDao: UserGameDataDao;
@@ -29,6 +32,7 @@ export class FirestoreDaoFactory implements DaoFactory {
     this.customerDao = new FirestoreCustomerDao(db);
     this.layoutDao = new FirestoreLayoutDao(db);
     this.slugDao = new FirestoreSlugDao(db);
+    this.storageDao = new FirebaseStorageDao(storage);
     this.upvoteDao = new FirestoreUpvoteDao(db);
     this.userDao = new FirestoreUserDao(db);
     this.userGameDataDao = new FirestoreUserGameDataDao(db);
@@ -60,5 +64,9 @@ export class FirestoreDaoFactory implements DaoFactory {
 
   public getCustomerDao(): CustomerDao {
     return this.customerDao;
+  }
+
+  public getStorageDao(): StorageDao {
+    return this.storageDao;
   }
 }
--- src/services/factory/IServiceFactory.ts
+++ src/services/factory/IServiceFactory.ts
@@ -1,5 +1,7 @@
 import type { ILayoutService } from "../interfaces/ILayoutService";
+import type { IUserService } from "../interfaces/IUserService";
 
 export interface IServiceFactory {
   getLayoutService: () => ILayoutService;
+  getUserService: () => IUserService;
 }
